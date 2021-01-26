7e0d7df848035d1274cb6e255861e66b45e6676f
ezreall <huythang1019@gmail.com>
2021-01-26 22:01:42 +0700

    baitap

--- login/login.php
+++ login/login.php
@@ -26,9 +26,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST["password"];
 
     if ($username === "admin" && $password === "admin") {
-        echo "<h2>Welcome <span style='color:red'>" .$username. "</span> to website</h2>";
-    } else{
-        echo "<h2><span style='color:red'>Login Error</span></h2>";
+        header("loation : xinchao.php");
+    } else if (($username === "admin") || ($password === "admin")){
+        echo "ban da nhap sai";
     }
 }
 ?>
